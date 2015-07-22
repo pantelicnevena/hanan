477eae84ca01aef98f2f1371c1ea70c47ad519b3
enco164 <enco164@gmail.com>
2015-07-22 13:38:07 +0200

    ispravljen zilla likes counter

--- wp-content/plugins/zilla-likes/scripts/zilla-likes.js
+++ wp-content/plugins/zilla-likes/scripts/zilla-likes.js
@@ -1,27 +1,28 @@
 jQuery(document).ready(function($){
 
 	$('.zilla-likes').live('click',
-	    function() {
-    		var link = $(this);
-    		if(link.hasClass('active')) return false;
-		
-    		var id = $(this).attr('id'),
-    			postfix = link.find('.zilla-likes-postfix').text();
-			
-    		$.post(zilla_likes.ajaxurl, { action:'zilla-likes', likes_id:id, postfix:postfix }, function(data){
-				$('.counter-desc').html(data);
-    			link.addClass('active').attr('title','You already like this').attr('disabled', 'disabled');
-    		});
-		
-    		return false;
-	});
-	
+		function() {
+			var link = $(this);
+			if(link.hasClass('active')) return false;
+
+			var array = $(this).attr('id').split('-');
+			var id = 'counter-desc-' + array[array.length - 1];
+			var postfix = link.find('.zilla-likes-postfix').text();
+
+			$.post(zilla_likes.ajaxurl, { action:'zilla-likes', likes_id:link.attr('id'), postfix:postfix }, function(data){
+				$(document.getElementById(id)).html(data);
+				link.addClass('active').attr('title','You already like this').attr('disabled', 'disabled');
+			});
+
+			return false;
+		});
+
 	if( $('body.ajax-zilla-likes').length ) {
-        $('.counter-desc').each(function(){
-    		var array = $(this).attr('id').text().split('-');
-            var id = 'zilla-likes-'+array[array.length - 1];
-    		$(this).load(zilla_likes.ajaxurl, { action:'zilla-likes', post_id:id });
-    	});
+		$('.counter-desc').each(function(){
+			var array = $(this).attr('id').split('-');
+			var id = 'zilla-likes-'+array[array.length - 1];
+			$(this).load(zilla_likes.ajaxurl, { action:'zilla-likes', post_id:id });
+		});
 	}
 
 });
